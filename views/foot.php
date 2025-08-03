2deeec6ea9872352c1f53d026df9edf2b76a3424
MoeBrowne <moebrowne@users.noreply.github.com>
2025-08-03 15:48:50 +0100

    Runs the hexgrid once the browser is idle

--- views/foot.php
+++ views/foot.php
@@ -1,6 +1,6 @@
 <script src="/assets/hexGrid.js"></script>
 <script>
-    hexGrid.init(document.getElementById("hexGridEl"));
+    window.requestIdleCallback(() => hexGrid.init(document.getElementById("hexGridEl")));
 </script>
 </body>
 </html>
