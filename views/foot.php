1fda52e74a261d3a4be77f80702dabfe15980e70
MoeBrowne <moebrowne@users.noreply.github.com>
2026-01-09 22:14:47 +0000

    Adds click handler for kbd elements

--- views/foot.php
+++ views/foot.php
@@ -2,5 +2,21 @@
 <script>
     window.requestIdleCallback(() => hexGrid.init(document.getElementById("hexGridEl")));
 </script>
+
+<script>
+document
+    .addEventListener(
+        'click',
+        (e) => {
+            if (e.target.nodeName !== 'KBD') {
+                return;
+            }
+
+            if (e.target.innerText.toLowerCase() === 'end') {
+                window.scrollBy(0, window.innerHeight)
+            }
+        }
+    );
+</script>
 </body>
 </html>
