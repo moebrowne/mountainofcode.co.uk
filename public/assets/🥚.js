0bcc70c90805ca95f7115f5a69739885b07c3273
MoeBrowne <moebrowne@users.noreply.github.com>
2026-05-08 17:35:16 +0100

    Adds more keyboard buttons

--- public/assets/🥚.js
+++ public/assets/🥚.js
@@ -9,5 +9,13 @@ document
             if (e.target.innerText.toLowerCase() === 'end') {
                 window.scrollBy(0, window.innerHeight)
             }
+
+            if (e.target.innerText.toLowerCase() === 'up') {
+                window.scrollBy(0, -25)
+            }
+
+            if (e.target.innerText.toLowerCase() === 'down') {
+                window.scrollBy(0, 25)
+            }
         }
     );
