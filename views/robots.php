202f36621958217a448b9d7ce958132d97c38ec4
MoeBrowne <moebrowne@users.noreply.github.com>
2026-05-31 15:12:15 +0100

    Adds a missing content type

--- views/robots.php
+++ views/robots.php
@@ -5,6 +5,8 @@ $handle = fopen(__DIR__ . '/../bots', 'a');
 fwrite($handle, $_SERVER['HTTP_USER_AGENT'] . PHP_EOL);
 fclose($handle);
 
+header('Content-Type: text/plain; charset=utf-8');
+
 ?>
 User-agent: *
 Disallow: /bad-bot
